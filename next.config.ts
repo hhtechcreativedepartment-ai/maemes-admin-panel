8ea42035e533f5a646ef4446044ea5236fad15aa
Aqib Awan <aqibawan28@gmail.com>
2026-07-20 18:50:59 +0500

    configure admin panel project root

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  turbopack: {
+    root: process.cwd(),
+  },
+};
+
+export default nextConfig;import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   /* config options here */
 };
 
